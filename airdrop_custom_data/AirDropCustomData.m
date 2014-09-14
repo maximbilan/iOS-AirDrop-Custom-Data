45bae87758d61d32092474bd8688e7125bf9d4bf
Maxim Bilan <maximb.mail@gmail.com>
2014-09-14 20:33:46 +0300

    Code review

--- airdrop_custom_data/AirDropCustomData.m
+++ airdrop_custom_data/AirDropCustomData.m
@@ -25,18 +25,21 @@
     return self.url;
 }
 
-- (id)activityViewController:(UIActivityViewController *)activityViewController itemForActivityType:(NSString *)activityType
+- (id)activityViewController:(UIActivityViewController *)activityViewController
+         itemForActivityType:(NSString *)activityType
 {
     return self.url;
 }
 
-- (NSString *)activityViewController:(UIActivityViewController *)activityViewController subjectForActivityType:(NSString *)activityType
+- (NSString *)activityViewController:(UIActivityViewController *)activityViewController
+              subjectForActivityType:(NSString *)activityType
 {
     return self.subject;
 }
 
 - (UIImage *)activityViewController:(UIActivityViewController *)activityViewController
-      thumbnailImageForActivityType:(NSString *)activityType suggestedSize:(CGSize)size
+      thumbnailImageForActivityType:(NSString *)activityType
+                      suggestedSize:(CGSize)size
 {
     if ([activityType isEqualToString:UIActivityTypeAirDrop]) {
         return [UIImage imageNamed:@"AppIcon"];
