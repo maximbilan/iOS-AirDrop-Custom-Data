8c1b0eced61ad0337dded43b46f2946a2d10e16e
Maxim Bilan <maximb.mail@gmail.com>
2014-09-14 11:48:05 +0300

    Added src files

---
+++ airdrop_custom_data/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  airdrop_custom_data
+//
+//  Created by Maxim Bilan on 9/14/14.
+//  Copyright (c) 2014 Maxim Bilan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ airdrop_custom_data/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  airdrop_custom_data
+//
+//  Created by Maxim Bilan on 9/14/14.
+//  Copyright (c) 2014 Maxim Bilan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
